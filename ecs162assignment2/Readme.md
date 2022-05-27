6003568ee2d7c7b3bda7dfae51e10af5b88f1e29
Tom Kwon <39015477+ForMeCodingIsLikeTypeRacer@users.noreply.github.com>
2022-05-26 20:33:50 -0700

    Add files via upload

---
+++ ecs162assignment2/Readme.md
@@ -0,0 +1,42 @@
+# ***Web_Dev Notes***<!-- omit in toc --> 
+- [Questions](#questions)
+  - [2/11](#211)
+- [Notes](#notes)
+  - [2/11](#211-1)
+- [To-do](#to-do)
+  - [Now](#now)
+  - [Soon](#soon)
+  - [Later](#later)
+- [Brainstorms](#brainstorms)
+
+# Questions
+
+## [2/11](#211-1)
+
+- 
+
+# Notes
+
+## [2/11](#211)
+
+- 
+
+# To-do
+
+## Now  
+
+- [ ] html 
+
+## Soon 
+
+- [ ]Javascript
+- [ ]CSS
+
+## Later 
+
+-  
+
+# Brainstorms 
+
+- 
+ 
---
+++ ecs162assignment2/exhibit/source/app.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    // alert("bingbong");
+    var x = document.getElementById("hider");
+    var j = document.getElementById('topbottommarg');
+    var m = document.getElementById('morebutton');
+    var q = document.getElementById('footerid');
+
+        x.style.display = "flex";
+        j.style.marginBottom = "0%";
+        m.style.display = "none";
+        q.style.visibility = "visible";
+ 
+}
+
+function backToNorm() {
+    // alert("bingbong");
+    var x = document.getElementById("hider");
+    var j = document.getElementById('topbottommarg');
+    var m = document.getElementById('morebutton');
+    var l = document.getElementById('lessbutton');
+    var q = document.getElementById('footerid');
+
+    x.style.display = "none";
+    j.style.marginBottom = "3%";
+    m.style.display = "flex";
+    q.style.visibility = "invisible";
+}
